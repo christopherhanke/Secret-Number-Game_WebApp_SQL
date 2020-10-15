4691d730fa40b89c191b937d68d30afe5ec86f90
christopherhanke <christopherhanke@googlemail.com>
2020-10-15 22:23:40 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,67 @@
+import random
+
+from datetime import datetime
+from flask import Flask, render_template, request, make_response, redirect, url_for
+from models import User, db
+
+
+# runtime environment starts
+app = Flask(__name__)
+db.create_all()
+
+
+# index page
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        name = request.form.get("user-name")
+        email = request.form.get("user-email")
+
+        user = db.query(User).filter_by(email=email).first()
+        if not user:
+            user = User(name=name, email=email, secret_number=random.randint(1, 30), solved=None)
+            db.add(user)
+            db.commit()
+
+        response = make_response(render_template("game.html"))
+        response.set_cookie("email", email)
+
+        return response
+
+    else:
+        return render_template("welcome.html")
+
+
+# game page and game routine
+@app.route("/game", methods=["GET", "POST"])
+def game():
+    if request.method == "POST":
+        email = request.cookies.get("email")
+        if not email:
+            return redirect(url_for("index"))
+
+        user = db.query(User).filter_by(email=email).first()
+
+        try:
+            guess = int(request.form.get("guess"))
+        except ValueError:
+            return render_template("game.html", no_int=True)
+
+        if guess < user.secret_number:
+            helpline = "Your guess is below the secret number."
+        elif guess > user.secret_number:
+            helpline = "Your guess is above the secret number."
+        else:
+            if not user.solved:
+                user.solved = datetime.now()
+                db.commit()
+            helpline = "Hooray"
+
+        return render_template("game.html", helpline=helpline)
+    else:
+        return render_template("game.html")
+
+
+# main routine to start the app
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,5 @@
+**SmartNinja Homework #18 - Secret Number Game WebApp with SQL **
+
+The Secret Number Game WebApp had to get redefined with an SQL Database,
+so that the Secret Number was no longer visible to the user via Cookie.
+SQL Database was implemented with SQLAlchemy. 
